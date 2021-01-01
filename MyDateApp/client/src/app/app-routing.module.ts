03c7deca1e533c7ad471c6439cfa8e2081a923ea
Tihomir Vasilev <rinzz86@yahoo.com>
2021-01-02 01:46:46 +0200

    feat:add route guard

--- MyDateApp/client/src/app/app-routing.module.ts
+++ MyDateApp/client/src/app/app-routing.module.ts
@@ -4,10 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'members', component: MemberDetailComponent },
+  {
+    path: 'members',
+    component: MemberDetailComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'members/:id', component: MemberDetailComponent },
   { path: 'lists', component: ListsComponent },
   { path: 'messages', component: MessagesComponent },
