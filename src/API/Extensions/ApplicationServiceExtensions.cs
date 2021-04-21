1e3e4caccabdc4e9554070bbd0539c93727b0002
Tihomir Vasilev <rinzz86@yahoo.com>
2021-04-22 00:00:51 +0300

    feat: add photo, edit photos, delete photos, upload to cloudinary

--- src/API/Extensions/ApplicationServiceExtensions.cs
+++ src/API/Extensions/ApplicationServiceExtensions.cs
@@ -1,12 +1,12 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
-using AutoMapper;
-using API.Helpers;
 
 namespace API.Extensions
 {
@@ -14,8 +14,9 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options => options.UseSqlite(config.GetConnectionString("DefaultConnection")));
